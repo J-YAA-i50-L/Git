610b25f2df60851d0c67b10dbe74bb7b86f1c90e
YudovAnton <yudovanton@yandex.ru>
2022-11-18 19:42:28 +0300

    Решил порешать

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-print('Мой первый проект в Git')
-print('Что хочу то и ворочу!')
-print(12345)
-d()
+s = [int(i) for i in input().split()]
+h, b = max(s) + 1, len(s) + 2
+s = [h - i for i in s]
 
+for i in range(h + 1):
+    if i == 0:
+        [print('*', end='') for _ in range(b)]
+        print()
+    else:
+        print('*', end='')
+        for j in range(b - 2):
+            if s[j] > 0:
+                print(' ', end='')
+            else:
+                print('*', end='')
+            s[j] = s[j] - 1
+        print('*', end='')
+        print()
 
-def d():
-    print([i * 2 + 5 for i in range(101)])
