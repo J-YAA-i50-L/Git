c8a67bb5f1e6adddcc5226444913e1f0741d2e76
YudovAnton <yudovanton@yandex.ru>
2022-11-21 16:00:02 +0300

    Решил порешать

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-s = [int(i) for i in input().split()]
-h, b = max(s) + 1, len(s) + 2
-s = [h - i for i in s]
-
-for i in range(h + 1):
-    if i == 0:
-        [print('*', end='') for _ in range(b)]
-        print()
-    else:
-        print('*', end='')
-        [print(' ', end='') if s[j] > 0 else print('*', end='') for j in range(b - 2)]
-        s = [s[j] - 1 for j in range(b - 2)]
-        print('*', end='')
-        print()
+def strip_punctuation_ru(data):
+    blok = [',', '.', ';', ':', '(', ')', '{', '}', '!', '?']
+    now_strok = ''
+    data = data.replace(' - ', ' ')
+    for count, elem in enumerate(data):
+        if elem not in blok:
+            now_strok += elem
+        elif elem in blok and len(data) > 1 and data[count + 1] != ' ':
+            now_strok += ' '
+    return now_strok.strip()
