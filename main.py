7cd146b02e4519abdc5847ec38e62291cc6d0510
YAA <yudovanton@yandex.ru>
2022-11-22 20:17:21 +0300

    Решил порешать

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-def strip_punctuation_ru(data):
-    blok = [',', '.', ';', ':', '(', ')', '{', '}', '!', '?']
-    now_strok = ''
-    data = data.replace(' - ', ' ')
-    for count, elem in enumerate(data):
-        if elem not in blok:
-            now_strok += elem
-        elif elem in blok and len(data) > 1 and data[count + 1] != ' ':
-            now_strok += ' '
-    return now_strok.strip()
+def is_correct_mobile_phone_ru(nomer):
+    if 11 == len(nomer):
+        if nomer[0] == '8':
+            return 'YES'
+        else:
+            return 'NO'
+    if 12 == len(nomer):
+        if nomer[:2] == '+7':
+            return 'YES'
+        else:
+            return 'NO'
+    elif len(nomer) > 12:
+        if nomer[0] == '8':
+            if (len(nomer) == 13 and nomer.find('(') == 1 and nomer.find(')') == 5
+               and nomer.count('(') == 1 and nomer.count(')') == 1):
+                return 'YES'
+            elif (len(nomer) == 15 and nomer.count('-') == 2 and nomer.count(' ') == 2
+                  and len(nomer[1:6].strip()) == 3 and nomer[9] == '-' and nomer[12] == '-'):
+                return 'YES'
+            else:
+                return 'NO'
+        if nomer[:2] == '+7':
+            if (len(nomer) == 14 and nomer.find('(') == 2 and nomer.find(')') == 6
+               and nomer.count('(') == 1 and nomer.count(')') == 1):
+                return 'YES'
+            elif (len(nomer) == 16 and nomer.count('-') == 2 and nomer.count(' ') == 2
+                  and len(nomer[2:7].strip()) == 3 and nomer[10] == '-' and nomer[13] == '-'):
+                return 'YES'
+            else:
+                return 'NO'
+    else:
+        return 'NO'
+
+
+print(is_correct_mobile_phone_ru(input()))
